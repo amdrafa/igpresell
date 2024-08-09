11920f74383dee0d7fdea2408eb52c9b892a3b5d
unknown <tmrafinha4@gmail.com>
2024-08-09 13:09:28 -0300

    page updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,57 @@ import porcentagem2 from "./assets/porcentagem2.png"
 import porcentagem3 from "./assets/porcentagem3.png"
 import porcentagem4 from "./assets/porcentagem4.png"
 import porcentagem5 from "./assets/porcentagem5.png"
+import roleta from "./assets/roleta.jpeg"
+import bonus from "./assets/ganhos.jpeg"
 
 
 
 function App() {
   return (
     <div>
-      <div className="flex justify-center bg-green-600 text-white font-extrabold py-2 text-base">
-        VOCÊ GANHOU 40 RODADAS GRÁTIS
+      <div className="flex justify-center bg-red-500 text-white  font-extrabold py-2 text-xl">
+        DEPÓSITO MÍNIMO: R$20
       </div>
       <main className='main-bg bg-black text-white'>
-        <div className='flex flex-col justify-center items-center px-2 mb-36'>
-          <span className='text-7xl mb-4'>
-            🔞
-          </span>
-          <div className='font-extrabold text-3xl text-center'>
-            VOCÊ TEM <span className='text-red-600'>+18 ANOS?</span> <br /> <br /> <span className='text-green-500'>DEPOSITE</span> PARA GANHAR UM <span className='text-orange-400'>BÔNUS</span> DE <span className='text-blue-600'>R$100</span>
+        <div className='flex flex-col justify-center items-center px-2 mt-2'>
+          <a href="https://4play.bet/affiliates/?btag=2042404">
+            <img width={250} src={roleta} alt="" />
+          </a>
+          <div className="font-extrabold text-2xl text-center">
+            DEPOSITE R$10 PRA{" "} <br />
+            <span className="text-transparent text-5xl bg-clip-text bg-gradient-to-r from-yellow-300 to-yellow-500">
+              GANHAR UM BÔNUS
+            </span>
           </div>
-          <div className='text-2xl text-center opacity-80 my-4 mb-8'>
+
+          <a href="https://4play.bet/affiliates/?btag=2042404" className='my-4'>
+            <img width={250} src={bonus} alt="" />
+          </a>
+
+          <div className='text-2xl text-center opacity-80 my-4 mb-8 px-4'>
             Clique no botão abaixo e finalize seu cadastro
           </div>
           <a href='https://4play.bet/affiliates/?btag=2042404' className='w-full px-5'>
-            <button className='text-zinc-700 w-full text-xl bg-white font-extrabold p-4 rounded-lg shadow-lg hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50 pulsar-brilho'>
+            <button className='text-white w-full text-xl bg-green-500 font-extrabold p-4 rounded-lg shadow-lg hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50 pulsar-brilho'>
               FINALIZE SEU CADASTRO
             </button>
           </a>
 
-          <div className='mt-8'>
+          <div className='font-extrabold mt-8 text-3xl'>
+            JOGOS <span className='text-yellow-400'>PAGANTES</span>
+          </div>
+          <div className='mb-4 text-2xl'>
             Arraste para o lado.
           </div>
 
-          <div className='overflow-auto flex'>
+          <a href="https://4play.bet/affiliates/?btag=2042404" className='overflow-auto flex mb-8'>
             <img width={150} src={porcentagem1} alt="" />
             <img width={150} src={porcentagem2} alt="" />
             <img width={150} src={porcentagem3} alt="" />
             <img width={150} src={porcentagem4} alt="" />
             <img width={150} src={porcentagem5} alt="" />
-          </div>
+          </a>
+
         </div>
       </main>
     </div>
