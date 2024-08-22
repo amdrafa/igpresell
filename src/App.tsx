54c5e0be955a0eeacb450359aefb176e122eb7a2
unknown <tmrafinha4@gmail.com>
2024-08-22 15:20:21 -0300

    back to root

--- src/App.tsx
+++ src/App.tsx
@@ -4,155 +4,59 @@ import porcentagem2 from "./assets/porcentagem2.png"
 import porcentagem3 from "./assets/porcentagem3.png"
 import porcentagem4 from "./assets/porcentagem4.png"
 import porcentagem5 from "./assets/porcentagem5.png"
-import bgigaming from "./assets/capa-ig.png"
-import telegram from "./assets/telegram.png"
-import whatsapp from "./assets/whatsapp.png"
+// import bgigaming from "./assets/capa-ig.png"
+// import telegram from "./assets/telegram.png"
+// import whatsapp from "./assets/whatsapp.png"
+import roleta from "./assets/roleta.jpeg"
+import bonus from "./assets/ganhos.jpeg"
 
 
 
 function App() {
-  // return (
-  //   <div>
-  //     <div className="flex justify-center bg-red-500 text-white  font-extrabold py-2 text-xl">
-  //       DEPÓSITO MÍNIMO: R$5
-  //     </div>
-  //     <main className='main-bg bg-black text-white'>
-  //       <div className='flex flex-col justify-center items-center px-2 mt-2'>
-  //         <a href="https://4play.bet/affiliates/?btag=2042404">
-  //           <img width={250} src={roleta} alt="" />
-  //         </a>
-  //         <div className="font-extrabold text-2xl text-center">
-  //           DEPOSITE R$10 PRA{" "} <br />
-  //           <span className="text-transparent text-5xl bg-clip-text bg-gradient-to-r from-yellow-300 to-yellow-500">
-  //             GANHAR UM BÔNUS
-  //           </span>
-  //         </div>
-
-  //         <a href="https://4play.bet/affiliates/?btag=2042404" className='my-4'>
-  //           <img width={250} src={bonus} alt="" />
-  //         </a>
-
-  //         <div className='text-2xl text-center opacity-80 my-4 mb-8 px-4'>
-  //           Clique no botão abaixo e finalize seu cadastro
-  //         </div>
-  //         <a href='https://4play.bet/affiliates/?btag=2042404' className='w-full px-5'>
-  //           <button className='text-white w-full text-xl bg-green-500 font-extrabold p-4 rounded-lg shadow-lg hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50 pulsar-brilho'>
-  //             FINALIZE SEU CADASTRO
-  //           </button>
-  //         </a>
-
-  //         <div className='font-extrabold mt-8 text-3xl'>
-  //           JOGOS <span className='text-yellow-400'>PAGANTES</span>
-  //         </div>
-  //         <div className='mb-4 text-2xl'>
-  //           Arraste para o lado.
-  //         </div>
-
-  //         <a href="https://4play.bet/affiliates/?btag=2042404" className='overflow-auto flex mb-8'>
-  //           <img width={150} src={porcentagem1} alt="" />
-  //           <img width={150} src={porcentagem2} alt="" />
-  //           <img width={150} src={porcentagem3} alt="" />
-  //           <img width={150} src={porcentagem4} alt="" />
-  //           <img width={150} src={porcentagem5} alt="" />
-  //         </a>
-
-  //         (
-  //         <footer className="bg-gray-800 text-gray-400 py-8">
-  //           <div className="container mx-auto px-4">
-  //             <div className="flex flex-col md:flex-row justify-between items-center">
-  //               <div className="mb-4 md:mb-0">
-  //                 <h2 className="text-lg font-semibold text-gray-100">IgSolutions</h2>
-  //                 <p className="text-sm">CNPJ: 12.345.678/0001-90</p>
-  //                 <p className="text-sm">Endereço: Rua Conceição, 253, Bairro São João, Itajaí - SC</p>
-  //                 <p className="text-sm">Telefone: (11) 1234-5678</p>
-  //                 <p className="text-sm">E-mail: contato@empresa.com.br</p>
-  //               </div>
-  //               <div className="mb-4 md:mb-0">
-  //                 <p className="text-sm">© {new Date().getFullYear()} IgSolutions. Todos os direitos reservados.</p>
-  //                 <p className="text-sm">Termos de Uso | Política de Privacidade</p>
-  //               </div>
-  //               <div className="flex space-x-4">
-  //                 <a href="https://www.facebook.com/IgSolutions" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
-  //                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-  //                     <path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.496v-9.294h-3.127v-3.622h3.127v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.464.099 2.795.143v3.239l-1.918.001c-1.505 0-1.796.715-1.796 1.763v2.312h3.59l-.467 3.622h-3.123v9.294h6.116c.731 0 1.324-.593 1.324-1.324v-21.35c0-.732-.593-1.325-1.324-1.325z" />
-  //                   </svg>
-  //                 </a>
-  //                 {/* Outros ícones de redes sociais podem ser adicionados aqui */}
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </footer>
-
-  //       </div>
-
-
-  //     </main>
-  //   </div>
-  // );
-
   return (
     <div>
-      <div className="flex justify-center bg-red-500 text-white py-2 text-xl">
-        43 Vagas gratúitas disponíveis.
+      <div className="flex justify-center bg-red-500 text-white  font-extrabold py-2 text-xl">
+        DEPÓSITO MÍNIMO: R$5
       </div>
       <main className='main-bg bg-black text-white'>
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center px-2 mt-2'>
           <a href="https://4play.bet/affiliates/?btag=2042404">
-            <img width={400} src={bgigaming} alt="" />
+            <img width={250} src={roleta} alt="" />
           </a>
-          {/* <div className='flex items-center'>
-            <a href="https://4play.bet/affiliates/?btag=2042404">
-              <img width={250} src={roleta} alt="" />
-            </a>
-            <a href="https://4play.bet/affiliates/?btag=2042404" className='my-4'>
-              <img width={250} src={bonus} alt="" />
-            </a>
-          </div> */}
-
-          <div className='font-extrabold mt-1 mb-2 text-3xl'>
-            <span className="text-transparent text-3xl bg-clip-text bg-gradient-to-r from-green-400 to-green-600">
-              + INDICAÇÕES PAGANTES
+          <div className="font-extrabold text-2xl text-center">
+            DEPOSITE R$10 PRA{" "} <br />
+            <span className="text-transparent text-5xl bg-clip-text bg-gradient-to-r from-yellow-300 to-yellow-500">
+              GANHAR UM BÔNUS
             </span>
           </div>
 
-          <div className='font-extrabold mt-1 mb-2 text-3xl'>
-            <span className="text-transparent text-3xl bg-clip-text bg-gradient-to-r from-green-400 to-green-600">
-              + ALAVANCAGENS
-            </span>
-          </div>
-
-          <div className='font-extrabold mt-1 mb-2 text-3xl'>
-            <span className="text-transparent text-3xl bg-clip-text bg-gradient-to-r from-green-400 to-green-600">
-              + ESTRATÉGIAS
-            </span>
-          </div>
-
-          <a href="https://4play.bet/affiliates/?btag=2042404" className='overflow-auto flex'>
-            <img width={120} src={porcentagem1} alt="" />
-            <img width={120} src={porcentagem2} alt="" />
-            <img width={120} src={porcentagem3} alt="" />
-            <img width={120} src={porcentagem4} alt="" />
-            <img width={120} src={porcentagem5} alt="" />
+          <a href="https://4play.bet/affiliates/?btag=2042404" className='my-4'>
+            <img width={250} src={bonus} alt="" />
           </a>
 
           <div className='text-2xl text-center opacity-80 my-4 mb-8 px-4'>
-            Quer ter acesso? <span className='font-bold'>Entra no grupo!</span>
+            Clique no botão abaixo e finalize seu cadastro
           </div>
-          <a href='https://chat.whatsapp.com/JhT2BxJ0VfT8SsojIMeNvW' className='w-full px-5 mb-4'>
-            <button className='text-white w-full text-xl bg-green-500 font-extrabold p-4 rounded-lg shadow-lg hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50 pulsar-brilho flex items-center justify-center space-x-2'>
-              <img width={50} src={whatsapp} alt="" />
-              <span>WHATSAPP</span>
+          <a href='https://4play.bet/affiliates/?btag=2042404' className='w-full px-5'>
+            <button className='text-white w-full text-xl bg-green-500 font-extrabold p-4 rounded-lg shadow-lg hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50 pulsar-brilho'>
+              FINALIZE SEU CADASTRO
             </button>
           </a>
 
-          <a href='https://t.me/+fG_lD1ywBMwxMWMx' className='w-full px-5 mb-10'>
-            <button className='text-white w-full text-xl bg-blue-400 font-extrabold p-4 rounded-lg shadow-lg hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50 pulsar-brilho flex items-center justify-center space-x-2'>
-              <img width={50} src={telegram} alt="" />
-              <span>TELEGRAM</span>
-            </button>
+          <div className='font-extrabold mt-8 text-3xl'>
+            JOGOS <span className='text-yellow-400'>PAGANTES</span>
+          </div>
+          <div className='mb-4 text-2xl'>
+            Arraste para o lado.
+          </div>
+
+          <a href="https://4play.bet/affiliates/?btag=2042404" className='overflow-auto flex mb-8'>
+            <img width={150} src={porcentagem1} alt="" />
+            <img width={150} src={porcentagem2} alt="" />
+            <img width={150} src={porcentagem3} alt="" />
+            <img width={150} src={porcentagem4} alt="" />
+            <img width={150} src={porcentagem5} alt="" />
           </a>
-
-
 
 
           <footer className="bg-gray-800 text-gray-400 py-8">
@@ -187,6 +91,104 @@ function App() {
       </main>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <div className="flex justify-center bg-red-500 text-white py-2 text-xl">
+  //       43 Vagas gratúitas disponíveis.
+  //     </div>
+  //     <main className='main-bg bg-black text-white'>
+  //       <div className='flex flex-col justify-center items-center'>
+  //         <a href="https://4play.bet/affiliates/?btag=2042404">
+  //           <img width={400} src={bgigaming} alt="" />
+  //         </a>
+  //         {/* <div className='flex items-center'>
+  //           <a href="https://4play.bet/affiliates/?btag=2042404">
+  //             <img width={250} src={roleta} alt="" />
+  //           </a>
+  //           <a href="https://4play.bet/affiliates/?btag=2042404" className='my-4'>
+  //             <img width={250} src={bonus} alt="" />
+  //           </a>
+  //         </div> */}
+
+  //         <div className='font-extrabold mt-1 mb-2 text-3xl'>
+  //           <span className="text-transparent text-3xl bg-clip-text bg-gradient-to-r from-green-400 to-green-600">
+  //             + INDICAÇÕES PAGANTES
+  //           </span>
+  //         </div>
+
+  //         <div className='font-extrabold mt-1 mb-2 text-3xl'>
+  //           <span className="text-transparent text-3xl bg-clip-text bg-gradient-to-r from-green-400 to-green-600">
+  //             + ALAVANCAGENS
+  //           </span>
+  //         </div>
+
+  //         <div className='font-extrabold mt-1 mb-2 text-3xl'>
+  //           <span className="text-transparent text-3xl bg-clip-text bg-gradient-to-r from-green-400 to-green-600">
+  //             + ESTRATÉGIAS
+  //           </span>
+  //         </div>
+
+  //         <a href="https://4play.bet/affiliates/?btag=2042404" className='overflow-auto flex'>
+  //           <img width={120} src={porcentagem1} alt="" />
+  //           <img width={120} src={porcentagem2} alt="" />
+  //           <img width={120} src={porcentagem3} alt="" />
+  //           <img width={120} src={porcentagem4} alt="" />
+  //           <img width={120} src={porcentagem5} alt="" />
+  //         </a>
+
+  //         <div className='text-2xl text-center opacity-80 my-4 mb-8 px-4'>
+  //           Quer ter acesso? <span className='font-bold'>Entra no grupo!</span>
+  //         </div>
+  //         <a href='https://chat.whatsapp.com/JhT2BxJ0VfT8SsojIMeNvW' className='w-full px-5 mb-4'>
+  //           <button className='text-white w-full text-xl bg-green-500 font-extrabold p-4 rounded-lg shadow-lg hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50 pulsar-brilho flex items-center justify-center space-x-2'>
+  //             <img width={50} src={whatsapp} alt="" />
+  //             <span>WHATSAPP</span>
+  //           </button>
+  //         </a>
+
+  //         <a href='https://t.me/+fG_lD1ywBMwxMWMx' className='w-full px-5 mb-10'>
+  //           <button className='text-white w-full text-xl bg-blue-400 font-extrabold p-4 rounded-lg shadow-lg hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50 pulsar-brilho flex items-center justify-center space-x-2'>
+  //             <img width={50} src={telegram} alt="" />
+  //             <span>TELEGRAM</span>
+  //           </button>
+  //         </a>
+
+
+
+
+  //         <footer className="bg-gray-800 text-gray-400 py-8">
+  //           <div className="container mx-auto px-4">
+  //             <div className="flex flex-col md:flex-row justify-between items-center">
+  //               <div className="mb-4 md:mb-0">
+  //                 <h2 className="text-lg font-semibold text-gray-100">IgSolutions</h2>
+  //                 <p className="text-sm">CNPJ: 12.345.678/0001-90</p>
+  //                 <p className="text-sm">Endereço: Rua Conceição, 253, Bairro São João, Itajaí - SC</p>
+  //                 <p className="text-sm">Telefone: (11) 1234-5678</p>
+  //                 <p className="text-sm">E-mail: contato@empresa.com.br</p>
+  //               </div>
+  //               <div className="mb-4 md:mb-0">
+  //                 <p className="text-sm">© {new Date().getFullYear()} IgSolutions. Todos os direitos reservados.</p>
+  //                 <p className="text-sm">Termos de Uso | Política de Privacidade</p>
+  //               </div>
+  //               <div className="flex space-x-4">
+  //                 <a href="https://www.facebook.com/IgSolutions" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white">
+  //                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+  //                     <path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.496v-9.294h-3.127v-3.622h3.127v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.464.099 2.795.143v3.239l-1.918.001c-1.505 0-1.796.715-1.796 1.763v2.312h3.59l-.467 3.622h-3.123v9.294h6.116c.731 0 1.324-.593 1.324-1.324v-21.35c0-.732-.593-1.325-1.324-1.325z" />
+  //                   </svg>
+  //                 </a>
+  //                 {/* Outros ícones de redes sociais podem ser adicionados aqui */}
+  //               </div>
+  //             </div>
+  //           </div>
+  //         </footer>
+
+  //       </div>
+
+
+  //     </main>
+  //   </div>
+  // );
 }
 
 export default App;
